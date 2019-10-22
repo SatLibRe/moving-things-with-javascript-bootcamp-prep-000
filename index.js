1608bd87b7fdd808ddaec811e4b26cdf6158a58d
free-writer-5650 <miles.higbie@gmail.com>
2019-10-22 00:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger');
 
-ocument.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
  
     dodger.style.left = `${left - 1}px`
   }
-})
+});
+
